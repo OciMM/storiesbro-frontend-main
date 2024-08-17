1c2d46d3b996a61182bb958d59c457ce19c4c193
OciMM <bekasovmaks20@gmail.com>
2024-08-17 17:03:34 +0300

    fix

--- src/components/AuthorizedAdmin/adminLeftSideBar/AdminLeftSideBar.jsx
+++ src/components/AuthorizedAdmin/adminLeftSideBar/AdminLeftSideBar.jsx
@@ -57,11 +57,11 @@ const AdminLeftSideBar = ({ open, setOpen }) => {
             </Typography>
           </Typography>
           <Box sx={{ width: "75%" }}>
-            {/* <MyButton options={{ background: "#E37E31", color: "white" }}>
+            <MyButton options={{ background: "#E37E31", color: "white" }}>
             <Link to="/cash" style={{ textDecoration: "none" }}>
               <Typography sx={{ color: "white" }}>Пополнить</Typography>
             </Link>
-            </MyButton> */}
+            </MyButton>
           </Box>
         </Box>
         <Box
