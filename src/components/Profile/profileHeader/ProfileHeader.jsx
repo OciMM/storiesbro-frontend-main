a9b19dd8d59141b97bf9e4275a0a17ca39d1b94c
OciMM <bekasovmaks20@gmail.com>
2024-08-21 17:31:18 +0300

    исправил шапку в настройках профиля

--- src/components/Profile/profileHeader/ProfileHeader.jsx
+++ src/components/Profile/profileHeader/ProfileHeader.jsx
@@ -18,91 +18,68 @@ const ProfileHeader = () => {
   const navigate = useNavigate();
 
   return (
-    <>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          p: { md: "0 130px", xs: 0 },
-        }}
-      >
-        <Box sx={{ display: "flex", alignItems: "center" }}>
-          <MenuIcon
-            sx={{ mr: { md: 5, xs: 0 }, cursor: "pointer" }}
-            onClick={() => setLeftSidebarOpen(true)}
-          />
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        p: { md: "0 130px", xs: "0 16px" },
+      }}
+    >
+      <Box sx={{ display: "flex", alignItems: "center" }}>
+        <MenuIcon
+          sx={{ mr: { md: 5, xs: 2 }, cursor: "pointer" }}
+          onClick={() => setLeftSidebarOpen(true)}
+        />
+        <LeftSideBar open={leftSidebarOpen} setOpen={setLeftSidebarOpen} />
 
-          <LeftSideBar open={leftSidebarOpen} setOpen={setLeftSidebarOpen} />
-          <RightSideBar open={rightSidebarOpen} setOpen={setRightSidebarOpen} />
-          {localStorage.getItem('statusAccount') == 'customer' && 
-            <Box
-              component="img"
-              alt="logo"
-              src={logo}
-              order={{ lg: 2, xs: 3 }}
-              item
-              lg={2}
-              xs={8}
-              sx={{
-                height: { xs: "50px", sm: "95px" },
-                width: { xs: "30px", sm: "60px" },
-                cursor: "pointer"
-              }}
-              onClick={() => navigate("/customer-help")}
-            />
-            }
-            {localStorage.getItem('statusAccount') == 'admin' && 
-              <Box
-                component="img"
-                alt="logo"
-                src={logo}
-                order={{ lg: 2, xs: 3 }}
-                item
-                lg={2}
-                xs={8}
-                sx={{
-                  height: { xs: "50px", sm: "95px" },
-                  width: { xs: "30px", sm: "60px" },
-                  cursor: "pointer"
-                }}
-                onClick={() => navigate("/admin-help")}
-              />
-            }
-        </Box>
         <Box
           component="img"
           alt="logo"
           src={logo}
-          sx={{ display: { xs: "block", lg: "none" } }}
-        />
-        <Box
           sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            width: { md: "15%", xs: "fit-content" },
+            height: { xs: "50px", sm: "95px" },
+            width: { xs: "30px", sm: "60px" },
+            cursor: "pointer",
           }}
-        >
-          <Box sx={{ width: "70%", display: { lg: "block", xs: "none" } }}>
-            {/* <MyButton
-              options={{ background: "#E68B46", color: "white", mr: 1 }}
-            >
+          onClick={() =>
+            navigate(
+              localStorage.getItem("statusAccount") === "customer"
+                ? "/customer-help"
+                : "/admin-help"
+            )
+          }
+        />
+      </Box>
+
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "flex-end",
+          alignItems: "center",
+          width: { md: "15%", xs: "fit-content" },
+        }}
+      >
+        <Box sx={{ width: "70%", display: { lg: "block", xs: "none" } }}>
+          {/* <MyButton
+            options={{ background: "#E68B46", color: "white", mr: 1 }}
+          >
             <Link to="/cash" style={{ textDecoration: "none" }}>
               <Typography sx={{ color: "white" }}>Пополнить</Typography>
             </Link>
-            </MyButton> */}
-          </Box>
-          <Box sx={{ display: { lg: "block", xs: "none" } }}>0р</Box>
-          <Box
-            component="img"
-            alt="prfile"
-            src={humanProfile}
-            onClick={() => setRightSidebarOpen(true)}
-            sx={{ cursor: "pointer" }}
-          />
+          </MyButton> */}
         </Box>
+        <Typography sx={{ display: { lg: "block", xs: "none" } }}>0₽</Typography>
+        <Box
+          component="img"
+          alt="profile"
+          src={humanProfile}
+          onClick={() => setRightSidebarOpen(true)}
+          sx={{ cursor: "pointer" }}
+        />
+        <RightSideBar open={rightSidebarOpen} setOpen={setRightSidebarOpen} />
       </Box>
-    </>
+    </Box>
   );
 };
 
