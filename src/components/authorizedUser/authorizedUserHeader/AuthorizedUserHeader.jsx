1d1c51873ca0bb2890fef3891d925c671d95e31f
OciMM <bekasovmaks20@gmail.com>
2024-09-02 00:50:07 +0300

    вернул старую шапку

--- src/components/authorizedUser/authorizedUserHeader/AuthorizedUserHeader.jsx
+++ src/components/authorizedUser/authorizedUserHeader/AuthorizedUserHeader.jsx
@@ -1,81 +1,90 @@
 import React, { useState } from "react";
-import { Box, Typography } from "@mui/material";
-import { Link, useNavigate } from "react-router-dom";
+import { Box, Grid, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 import logo from "./images/logo.svg";
 import menu from "./images/menu.svg";
 import AdminLeftSideBar from "../../AuthorizedAdmin/adminLeftSideBar/AdminLeftSideBar";
 import ExitMenu from "./exitMenu/ExitMenu";
 import AuthorizedCustomerLeftSidebar from "../../AuthorizedCustomer/authorizedCustomerLeftSidebar/AuthorizedCustomerLeftSidebar";
-import MyButton from "../../UI/buttons/MyButton";
 
 const AuthorizedUserHeader = ({ ismainpage = false, isCustomer }) => {
   const [open, setOpen] = useState(false);
   const [leftSidebarOpen, setLeftSidebarOpen] = useState(false);
 
   const navigate = useNavigate();
-  const accountStatus = localStorage.getItem("statusAccount");
-  const navigateToHelp = () =>
-    navigate(accountStatus === "customer" ? "/customer-help" : "/admin-help");
+  const accountStatus = localStorage.getItem('statusAccount');
+  const navigateToHelp = () => navigate(accountStatus === 'customer' ? "/customer-help" : "/admin-help");
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        p: { md: "0 130px", xs: "0 16px" },
-        height: { xs: "50px", sm: "auto" },
-      }}
-    >
-      {/* Иконка меню для мобильных устройств */}
-      <Box
-        component="img"
-        alt="menu"
-        src={menu}
-        sx={{
-          display: { xs: "block", lg: "none" },
-          cursor: "pointer",
-          height: { xs: "50px" },
-        }}
-        onClick={() => setLeftSidebarOpen(true)}
-      />
-      
+    <Grid container sx={{ alignItems: "center", justifyContent: "space-between" }}>
       {/* Логотип */}
-      <Box
+      <Grid
         component="img"
         alt="logo"
         src={logo}
+        item
+        lg={2}
+        xs={8}
+        order={{ xs: 2, lg: 1 }}
         sx={{
           height: { xs: "50px", sm: "95px" },
           width: { xs: "30px", sm: "60px" },
           cursor: "pointer",
-          ml: { xs: "16px", lg: "0" },
         }}
         onClick={navigateToHelp}
       />
 
-      {/* Средняя часть (например, для Switcher) */}
-      <Box
+      {/* Иконка меню */}
+      <Grid
+        item
+        xs={2}
         sx={{
+          height: { xs: "50px" },
+          display: {
+            xs: "block",
+            lg: "none",
+          },
+          cursor: "pointer",
+        }}
+        component="img"
+        alt="menu"
+        src={menu}
+        order={{ xs: 1 }}
+        onClick={() => setLeftSidebarOpen(true)}
+      />
+ 
+      {/* Средняя часть (например, для Switcher) */}
+      <Grid
+        item
+        lg={8}
+        xs={12}
+        order={{ xs: 3, lg: 2 }}
+        sx={{
+          background: {
+            xs: "white",
+            lg: "white",
+          },
+          height: { xs: "50px", sm: "auto" },
           display: ismainpage ? "flex" : "none",
           justifyContent: "center",
           alignItems: "center",
-          flexGrow: 1,
-          background: { xs: "#FBECE0", lg: "white" },
-          height: { xs: "50px", sm: "auto" },
         }}
       >
         {/* <Switcher ismainpage={ismainpage} /> */}
-      </Box>
+      </Grid>
 
-      {/* Правая часть */}
-      <Box
+      {/* Правая часть (выход и баланс) */}
+      <Grid
+        item
+        lg={ismainpage ? 2 : 10}
+        xs={2}
+        order={{ xs: 4, lg: 3 }}
         sx={{
           display: "flex",
           alignItems: "center",
           justifyContent: "flex-end",
-          width: { md: "15%", xs: "fit-content" },
+          position: "relative",
         }}
       >
         <Box
@@ -84,24 +93,17 @@ const AuthorizedUserHeader = ({ ismainpage = false, isCustomer }) => {
             display: { xs: "none", lg: "block" },
           }}
         >
-          <MyButton options={{ background: "#E37E31", color: "white" }}>
+          {/* <MyButton options={{ background: "#E37E31", color: "white" }}>
             <Link to="/cash" style={{ textDecoration: "none" }}>
               <Typography sx={{ color: "white" }}>Пополнить</Typography>
             </Link>
-          </MyButton>
+          </MyButton> */}
         </Box>
         <Typography sx={{ display: { xs: "none", lg: "block" } }}>
           0₽
         </Typography>
-        <Box
-          component="img"
-          alt="profile"
-          src={logo} // замените это на нужный вам путь к иконке профиля
-          onClick={() => setOpen(true)}
-          sx={{ cursor: "pointer", ml: 2 }}
-        />
         <ExitMenu open={open} setOpen={setOpen} />
-      </Box>
+      </Grid>
 
       {/* Боковые меню */}
       <AdminLeftSideBar
@@ -112,7 +114,7 @@ const AuthorizedUserHeader = ({ ismainpage = false, isCustomer }) => {
         open={isCustomer && leftSidebarOpen}
         setOpen={setLeftSidebarOpen}
       />
-    </Box>
+    </Grid>
   );
 };
 
